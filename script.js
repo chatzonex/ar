0c77b9af21a27ef20e2989648b135c0cee65fe63
chatzonex <chatzone20@gmail.com>
2026-08-25 13:02:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const splash = document.getElementById('splash');
-    const loadBar = document.getElementById('loadBar');
-
-    requestAnimationFrame(() => {
-        loadBar.style.transition = 'width 1.8s cubic-bezier(.4, 0, .2, 1)';
-        loadBar.style.width = '100%';
-    });
 
     setTimeout(() => {
         splash.classList.add('fade-out');
